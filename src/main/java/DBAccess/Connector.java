2d2c67fd6f696dcc1bbf34170b640e2f5e858720
Mikkel Kornval Christoffersen <mikkel@Mikkels-MacBook-Pro.local>
2019-03-19 08:51:55 +0100

    DBConnector update

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -15,7 +15,7 @@ public class Connector {
     //Constants
     private static final String IP = "localhost";
     private static final String PORT = "3306";
-    public static final String DATABASE = "cupcake";
+    public static final String DATABASE = "LegoHouse";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
